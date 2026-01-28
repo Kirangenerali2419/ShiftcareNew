2f2447f37feb928a82df0552d438ecbb6f01534a
Kirangenerali2419 <kiran.kumar.ext@generali.com.my>
2026-01-29 00:51:42 +0530

    updated android config

--- app.config.js
+++ app.config.js
@@ -19,6 +19,7 @@ export default {
       supportsTablet: true
     },
     android: {
+      package: "com.anonymous.shiftcareappointments",
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#FFFFFF'
